1823ea79190e67a467ab77908dea4ba6929d59d2
Tomasz John <mail@thomasjohn.eu>
2022-11-07 14:17:33 +0100

    test: async function onFormSend added

--- src/components/app.test.tsx
+++ src/components/app.test.tsx
@@ -4,6 +4,7 @@ import {
   screen,
   fireEvent,
   waitForElementToBeRemoved,
+  waitFor,
 } from "@testing-library/react";
 import App from "./App";
 
@@ -19,14 +20,14 @@ test("renders no data", () => {
   expect(table).toBeInTheDocument();
 });
 
-test("waiting for data", () => {
+test("waiting for data", async () => {
   render(<App />);
   const accountNameInput = screen.getByTestId("id-account-name");
   const getDataButton = screen.getByTestId("id-get-data");
   fireEvent.change(accountNameInput, { target: { value: "thomasjohn" } });
   fireEvent.click(getDataButton);
-  setTimeout(() => {
+  await waitFor(() => {
     const table3 = screen.getByTestId("id-result-has-data");
-    expect(table3).toHaveAttribute("data-testid", "id-result-has-data");
-  }, 1000);
+    expect(table3).toBeInTheDocument();
+  });
 });
